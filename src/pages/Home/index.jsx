e0cbe2bcd6010c0f45e227002f6bc4d1edba4f53
JulieBourelDrapier <juliebourel@hotmail.com>
2024-02-02 14:49:16 +0100

    deletion of useless files

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
-import logo from './logo.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
         <p>
           welcome to my app
         </p>
