155173d5be755d7f359e90b1c10513c85d43104c
JulieBourelDrapier <juliebourel@hotmail.com>
2024-02-05 14:40:38 +0100

    nav adapt (careful with links) + styled components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <nav className="header">
-      <Link to="#">Accueil</Link> 
-      <Link to="#about">Compétences</Link>
-      <Link to="#projects">Projets</Link>
-      <Link to="#contact">Contact</Link>
+      <Link to="/">Accueil</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/skills">Compétences</Link>
+      <Link to="/projets">Projets</Link>
     </nav>
   )
 }
+
 
 export default Header
